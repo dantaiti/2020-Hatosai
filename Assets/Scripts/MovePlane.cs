ab4895bd155804ec25ebc25385a99867684f6cea
dantaiti <danbansan0421@gmail.com>
2020-09-13 20:23:07 +0900

    カメラの移動　全身を追加

---
+++ Assets/Scripts/MovePlane.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MovePlane : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += Vector3.forward*speed;
+    }
+}
