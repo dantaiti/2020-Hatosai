d1e5916185b6d0b6b56568767d6bac819515de53
dantaiti <danbansan0421@gmail.com>
2020-11-17 20:45:46 +0900

    タイトルシーンの追加

---
+++ Assets/Scripts/TitleAnim.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.Serialization;
+
+public class TitleAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool _animeStart;
+   
+     [SerializeField ]private DOTweenVisualManager manager;
+     [SerializeField ]private DOTweenVisualManager textanimmanager;
+     [SerializeField] private ParticleSystem rightParticleSystem;
+     [SerializeField] private ParticleSystem leftParticleSystem;
+    void Start()
+    {
+        
+        _animeStart = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            manager.enabled = true;
+           
+        }
+    }
+
+   public void Test()
+    {
+        Debug.Log("PLAY!");
+       
+    }
+
+   public void OnParticle()
+   {
+       rightParticleSystem.Play();
+       leftParticleSystem.Play();
+
+   }
+}
